220b7877b303daa3653d4a179d238f946fa922a8
Andrew Shuvalov <andrew.shuvalov@mongodb.com>
2020-10-24 12:32:31 +0000

    project setup

---
+++ example-simple/simple.ts
@@ -0,0 +1,31 @@
+import { Machine, createMachine, assign } from 'xstate';
+
+const fetchMachine = Machine({
+    id: 'fetch',
+    initial: 'idle',
+    context: {
+      retries: 0
+    },
+    states: {
+      idle: {
+        on: {
+          FETCH: 'loading'
+        }
+      },
+      loading: {
+        on: {
+          RESOLVE: 'success',
+          REJECT: 'failure'
+        }
+      },
+      success: {
+        type: 'final'
+      },
+      failure: {
+        on: {
+          RETRY: 'loading'
+        }
+      }
+    }
+  });
+  
